1eafd73c26c35820c41e783ba7c0d255710709a5
Konstantin Martini <k@tuxcode.org>
2014-03-21 01:45:27 +0100

    added decorator to hook cmds

--- src/flowlib/flow.py
+++ src/flowlib/flow.py
@@ -1,3 +1,18 @@
+#from functools import wraps
+from inspect import isfunction
+
+cmd_reg = {}
+
+def expose(name):
+    def wrap(func):
+        cmd_reg[memo] = func
+        return func
+    if isfunction(name):
+        memo = name.__name__
+        return wrap(name)
+    else:
+        memo = name
+        return wrap
 
 class Flow(object):
     '''Base class for flows'''
--- src/flowlib/host.py
+++ src/flowlib/host.py
@@ -7,10 +7,37 @@ class Localhost(Host):
 
     def __init__(self):
         self.sh = sh
+        self._sudo = sh.sudo.bake()
+        self._cp = sh.cp.bake("-n")
 
     def run(self, command):
         '''emulate fabric.api.run'''
-        return self.sh(command.split())
+        result = self.sh(command.split())
+        return result
 
+    def sudo(self, command):
+        '''emulate fabric.api.sudo'''
+        result = self._sudo(command.split())
+        return result
+
+    def put(self, source, dest):
+        result = self._cp(source, dest)
+        return result
+
+    def get(self, source, dest):
+        result = self._cp(source, dest)
+        return result
+
+
+class RemoteHost(Host):
+
+    def __init__(self, name, user=None, cwd=None):
+        if user is None and not "@" in name:
+            name = "%s@%s" % (user, name)
+        elif user is not None:
+            name = "%s@%s" % (user, name)
+        if cwd is not None:
+            name = "%s:%s" % (name, cwd)
+        self.connstr = name
 
 # next up: implement remote hosts via fabric
