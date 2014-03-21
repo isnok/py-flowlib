a423c768b80e48b2313a9920d14d046d4a4e49ad
Konstantin Martini <k@tuxcode.org>
2014-03-21 02:40:50 +0100

    test commands

---
+++ src/flowlib/test_flow.py
@@ -0,0 +1,11 @@
+from flowlib.flow import expose
+
+@expose("alias")
+def aliased():
+    # an undocumented command
+    print "this is aliased"
+
+@expose
+def aliased():
+    """A documented command"""
+    print "this is weird"
