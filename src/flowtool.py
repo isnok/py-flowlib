c137195dabe000c1e3f52726a2cf4b024e1e59c8
Konstantin Martini <k@tuxcode.org>
2014-03-21 02:40:20 +0100

    upgrade cmdlinetool

--- src/flowtool.py
+++ src/flowtool.py
@@ -2,10 +2,12 @@
 '''flowtool - code flow manager
 
 Usage:
-    flowtool.py [-hl] [COMMAND] [ARGUMENTS ...]
+    flowtool.py [-hd] [COMMAND] [ARGUMENTS ...]
+    flowtool.py [-hd] [ --list | -l ]
 
     -h, --help          print this help
     -l, --list          list available commands
+    -d, --debug         print debug information
 
 Invoking without any arguments dumps the config.
 '''
@@ -14,4 +16,34 @@ from docopt import docopt
 
 args = docopt(__doc__)
 
-print args
+if args['--debug']:
+    print args
+
+##
+#  import custom flows here
+##
+
+import flowlib.test_flow
+
+
+from flowlib.flow import cmd_reg
+
+if args['--list']:
+    print "Available commands:"
+    for cmd, func in cmd_reg.iteritems():
+        print "    %-16s%s" % (cmd, func.__doc__)
+    import sys
+    sys.exit(0)
+
+
+command = args['COMMAND']
+arguments = args['ARGUMENTS']
+
+if command in cmd_reg:
+    result = cmd_reg[command](*arguments)
+    if result:
+        print "Command resulted:"
+        print result
+else:
+    print "Unknown command:", command
+    print "Use --list to discover available commands."
