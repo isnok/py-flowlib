51f2c6fbe51ca354c779e81890c80016d6d85c3c
Konstantin Martini <k@tuxcode.org>
2014-03-21 23:14:16 +0100

    should make more tests, but no good ideas right now

--- src/tests/test_args.py
+++ src/tests/test_args.py
@@ -7,14 +7,26 @@ tool_name = './ft.py'
 def tool():
     return getattr(sh, tool_name)
 
-def test_invoke(tool):
-    assert tool()
+_cache = {}
+@pytest.fixture
+def cached(tool):
+    if not _cache:
+        return _cache.setdefault(0, tool())
+    else:
+        return _cache[0]
 
-@pytest.mark.parametrize('test_config',
-        ['flows/test/flow.cfg', 'flows/simple/flow.cfg'])
-def test_config(tool, test_config):
-    one = tool()
-    two = tool('-c', test_config)
-    assert one
-    assert two
-    assert one != two
+@pytest.mark.parametrize('test_config', [
+    'flows/test/flow.cfg',
+    'flows/simple/flow.cfg',
+])
+def test_config(tool, cached, test_config):
+    confed = tool('-c', test_config)
+    assert confed
+    assert confed.exit_code == 0
+    assert confed.stdout != cached.stdout
+
+def test_debug(tool, cached):
+    debuged = tool('-d')
+    assert debuged
+    assert debuged.exit_code == 0
+    assert debuged.stdout != cached.stdout
