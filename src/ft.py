b04b7a588272ce7ba660070d84a63d12e6431816
Konstantin Martini <k@tuxcode.org>
2014-03-23 15:10:50 +0100

    added dirs and gits arg

--- src/ft.py
+++ src/ft.py
@@ -2,7 +2,7 @@
 '''flowtool - code flow manager
 
 Usage:
-    flowtool.py [-hdr] [-c <cfg>] [--nofmt] [-v...] [-n] [-H <host>] [-P <path>] [-o <file>]... [COMMAND] [ARGUMENTS ...]
+    flowtool.py [-hdr] [-c <cfg>] [--nofmt] [-v...] [-n] [-H <host>] [-P <path>] [-D <dirs>] [-G <gits>] [-o <file>]... [COMMAND] [ARGUMENTS ...]
     flowtool.py [-hdr] [-c <cfg>] [--nofmt] [-v...] [--list|-l]
 
 Options:
@@ -17,19 +17,23 @@ Options:
     --nofmt                 don't format (and color) output
 
 Flow hooks:
-
     -H, --host <host>       override default and config host [default: ]
     -P, --path <path>       override default and config repo [default: .]
+    -D, --dirs <dirs>       set default behaviour if dirs do not exist [default: abort]
+    -G, --gits <gits>       set default behaviour if gits do not exist [default: abort]
 
-Invoking without any arguments dumps the config.
+Invoking flowtool without any arguments dumps the config.
 '''
-import os, sys
+DIR_MODES = ['abort', 'create', 'warn', 'ignore']
+GIT_MODES = ['abort', 'create', 'warn', 'ignore', 'init']
+doc = __doc__.format(*[" | ".join(x) for x in (DIR_MODES, GIT_MODES)])
 
+import sys
 cmd_name = sys.argv[0]
 cmd_args = sys.argv[1:]
 
 import flib.env
-args = flib.env.parse_global_args(__doc__, cmd_args)
+args = flib.env.parse_global_args(doc, cmd_args)
 
 from flib.output import configure_logger
 log = configure_logger(cmd_name)
@@ -39,6 +43,7 @@ log.debug(args)
 if not args.recurse:
     config = flib.env.parse_config(args.config)
 else:
+    import os
     curdir = os.path.abspath(os.curdir)
     olddir = None
     while curdir != olddir:
