6ccc991e1e54237d3b0abe8f262c2faddbbb64ee
Konstantin Martini <km@nexttuesday.de>
2014-03-26 17:12:38 +0100

    v0.6.6a

--- src/setup.py
+++ src/setup.py
@@ -20,7 +20,7 @@ flowcfgs = ['simple.cfg', 'test.cfg']
 
 setup(
     name='flowlib',
-    version='0.6.5',
+    version='0.6.6a',
     description='Build your own git flow!',
     author='Konstantin Martini',
     author_email='k@tuxcode.org',
