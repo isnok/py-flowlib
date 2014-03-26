e11790c03b137aa0a2008df880fdec5b8c13805c
Konstantin Martini <km@nexttuesday.de>
2014-03-26 14:23:44 +0100

    correctly including flow cfgs

--- src/setup.py
+++ src/setup.py
@@ -4,13 +4,18 @@
 from setuptools import setup
 
 import os
+
+def relname(*args):
+    return os.path.join(os.path.dirname(__file__), *args)
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(relname(fname)).read()
 
+flowcfgs = ['simple.cfg', 'test.cfg']
 
 setup(
     name='flowlib',
@@ -21,8 +26,11 @@ setup(
     url='https://github.com/isnok/py-flowlib',
     scripts=['ft.py'],
     packages=['flib', 'flib.flows'],
-    data_files=[('', ['README'])],
-    #package_data={'':['README.md']},
+    data_files=[
+        ('', ['README']),
+        ('flib.flows', [relname('flib', 'flows', f) for f in flowcfgs]),
+    ],
+    package_data={'flib.flows':flowcfgs},
     #include_package_data=True,
     install_requires=['Fabric', 'configobj', 'docopt', 'sh'],
     keywords=['git','flow','shell','local','remote','commandline'],
