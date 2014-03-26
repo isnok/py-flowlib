dd24851abbf653920ad25eeac50e88ac14a66890
Konstantin Martini <km@nexttuesday.de>
2014-03-26 14:30:08 +0100

    add online 0.6.3 tarball and bump dev to next vesion

--- src/setup.py
+++ src/setup.py
@@ -19,7 +19,7 @@ flowcfgs = ['simple.cfg', 'test.cfg']
 
 setup(
     name='flowlib',
-    version='0.6.3',
+    version='0.6.4',
     description='Build your own git flow!',
     author='Konstantin Martini',
     author_email='k@tuxcode.org',
