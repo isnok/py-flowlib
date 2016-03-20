77bcf8d8b01fe74a245d6397f29527300d629f55
Konstantin Martini <konstantin.martini@zalando.de>
2016-03-20 13:10:35 +0100

    added commands to info command

--- src/flowtool/base/flowtool/info.py
+++ src/flowtool/base/flowtool/info.py
@@ -2,9 +2,18 @@ import sys
 import click
 from flowtool.style import colors, echo
 
+from pkg_resources import iter_entry_points
+
+def get_commands():
+    return [e.name for e in iter_entry_points('flowtool_commands')]
+
 @click.command()
 def show_info():
+    """ Gather and show general info. """
     echo.bold(' - flowtool info dump -')
     echo.bold()
     echo.magenta('python executable:', colors.yellow(sys.executable))
     echo.magenta('python version:', colors.yellow(str(sys.version_info)))
+    echo.bold()
+    commands = ', '.join([colors.cyan(c) for c in get_commands()])
+    echo.magenta('flowtool_commands:', commands)
