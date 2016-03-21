cf7baa34d2a84d770c95be7fb6beddc3a63c4947
Konstantin Martini <konstantin.martini@zalando.de>
2016-03-21 21:25:44 +0100

    needed a few hacky tricks to set this up...

--- base/flowtool/_version.py
+++ base/flowtool/_version.py
@@ -35,11 +35,11 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440   # TAG[+DISTANCE.gSHORTHASH[.dirty]]"
+    cfg.style = "pep440"
     cfg.tag_prefix = "flowtool-base-"
-    cfg.parentdir_prefix = "flowtool-base-"
-    cfg.versionfile_source = "flowtool/_version.py"
-    cfg.verbose = False
+    cfg.parentdir_prefix = "base"
+    cfg.versionfile_source = "base/flowtool/_version.py"
+    cfg.verbose = True
     return cfg
 
 
