8da587bb62e1d873e5d3a46a91ee5ff57b6f9a11
Konstantin Martini <k@martini.pm>
2016-03-25 00:54:56 +0100

    created git commons component

---
+++ git/flowtool_git/common.py
@@ -0,0 +1,43 @@
+import sys
+import click
+from flowtool.stlye import colors, echo
+from functools import wraps
+from collections import namedtuple
+
+__contents__ = ['local_repo', 'local_git_command']
+
+Cached = namedtuple('Cached', ['func_name', 'args', 'kwd'])
+
+_cache = {}
+def cached(func):
+    """ Decorator to cache function names and arguments in a plain dict.
+        May slow down evaluation if keyword dicts in function calls gets huge...
+    """
+    global _cache
+
+    name = func.__name__
+    @wraps(func)
+    def cached_func(*args, **kwd):
+        kwd_frozen = tuple(sorted(kwd.items()))
+        key = Cached(name, args, kwd_frozen)
+        if key in _cache:
+            return _cache[key]
+        else:
+            return _cache.setdefault(key, func(*args, **kwd))
+
+    return cached_func
+
+
+import git as gitpython
+
+@cached
+def local_repo(or_exit=True):
+    try:
+        return gitpython.Repo(search_parent_directories=True)
+    except gitpython.repo.base.InvalidGitRepositoryError as ex:
+        if or_exit:
+            echo.red('The current directory is not under git version control:', ex)
+            sys.exit(1)
+
+def local_git_command(*args, **kwd):
+    return local_repo(*args, **kwd).git
---
+++ git/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
+import os
+
+# General Info
+
+setup_args = dict(
+    name='flowtool-git',
+    version='0.7.1',
+    description='flowtool library of git related commands.',
+    author='Konstantin Martini',
+    author_email='k@tuxcode.org',
+    url='https://github.com/isnok/py-flowlib',
+)
+
+
+# Data Files
+
+setup_args.update(
+    packages=find_packages(),
+    zip_safe=False,
+)
+
+
+# Requirements
+
+setup_args.update(
+    install_requires=[
+        'click',
+        'flowtool-base',
+        'gitpython',
+    ],
+)
+
+
+# Entry Points
+
+setup_args.update(
+    entry_points={
+        'flowtool_commands': [
+            'co = flowtool_gitflow.basic:checkout_branch',
+            'feature = flowtool_gitflow.feature:main',
+            'ci = flowtool_gitflow.feature:commit',
+        ],
+    },
+)
+
+
+# Techincal Details
+
+setup_args.update(
+    keywords=['git','flow','shell','local','remote','commandline'],
+    platforms=['Debian/GNU Linux'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        #"License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: System :: Software Distribution",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+        #"Topic :: Internet",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Environment :: Console",
+    ],
+)
+
+setup(**setup_args)
