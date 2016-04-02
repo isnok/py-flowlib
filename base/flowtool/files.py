db24fe9fddbd31074148c1a69acf1c0cccb6af0e
Konstantin Martini <k@martini.pm>
2016-04-02 20:03:07 +0200

    added import_file functionality

--- base/flowtool/files.py
+++ base/flowtool/files.py
@@ -88,3 +88,27 @@ def cached_read(path):
 
 def cached_readlines(path):
     return _read_cache(path)[:-1]
+
+
+def import_file(name, path):
+    try: # py3.5
+        import importlib.util
+        spec = importlib.util.spec_from_file_location(name, path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+    except:
+        pass
+
+    try: # py3.3, py3.4
+        from importlib.machinery import SourceFileLoader
+        module = SourceFileLoader(name, path).load_module()
+        return module
+    except:
+        pass
+
+    try: # py2
+        import imp
+        return imp.load_source(name, path)
+    except:
+        pass
