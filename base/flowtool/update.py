b76903e0ca8f2626b87d75e0a4793b5f0402e947
Konstantin Martini <k@martini.pm>
2016-04-02 22:32:55 +0200

    self update to the max

--- base/flowtool/update.py
+++ base/flowtool/update.py
@@ -18,23 +18,31 @@ def update_installed(yes=None, identifiers=()):
     if identifiers:
         all_installed = [d.project_name for d in get_extensions()]
         to_upgrade = contains_any_filter(all_installed, *identifiers)
-        message = 'Upgrade these packages? ' + colors.cyan(', '.join(to_upgrade))
-        yes or click.confirm(
-            message,
-            default=True,
-            abort=True,
-        )
+        if to_upgrade:
+            message = 'Upgrade these packages? ' + colors.cyan(', '.join(to_upgrade))
+            yes or click.confirm(
+                message,
+                default=True,
+                abort=True,
+            )
+        else:
+            patterns = ', '.join([colors.cyan(i) for i in identifiers])
+            echo.yellow('Nothing matched any of these patterns:', patterns)
     elif yes:
         to_upgrade = [d.project_name for d in get_extensions()]
     else:
         to_upgrade = []
         for dist in get_extensions():
             if click.confirm(' '.join([
-                colors.white('== Update'),
+                colors.white('== Add'),
                 colors.cyan(dist.project_name),
-                colors.white('?')
+                colors.white('to update?')
             ]), default=True):
                 to_upgrade.append(dist.project_name)
+
+    if not to_upgrade:
+        echo.cyan('Nothing to upgrade.')
+        sys.exit()
 
     pip_args = ['--no-cache-dir', 'install', '--upgrade'] + to_upgrade
 
