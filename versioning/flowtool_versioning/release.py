bb6a25a18b3c9571dba54acdcdabf345573334bf
Konstantin Martini <k@martini.pm>
2016-04-02 20:04:10 +0200

    towards a nice release flow...

--- versioning/flowtool_versioning/release.py
+++ versioning/flowtool_versioning/release.py
@@ -11,8 +11,13 @@ def do_release():
     versioning = import_file('versioning', 'versioning.py')
     auto_version = versioning.get_version()
     dirty = 'dirty' in auto_version
+
     if dirty:
         echo.bold('Tag-Version check failed:', colors.red(auto_version))
         echo.cyan('You have to commit all changes before releasing.')
         sys.exit(1)
+
+    echo.bold('Tag-Version check passed:', colors.green(auto_version))
+    echo.bold('Bumping version.')
+    echo.white(run_command('./setup.py bump'))
 
