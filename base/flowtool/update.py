3093254b1c395fc53018bb18c07f71da3d559fe3
Konstantin Martini <k@martini.pm>
2016-04-02 19:02:17 +0200

    add -y option to self-update

--- base/flowtool/update.py
+++ base/flowtool/update.py
@@ -1,24 +1,35 @@
 import click
 import pip
+import sys
 
 from flowtool.style import echo, colors
 from flowtool.info import get_extensions
 
 @click.command()
-def update_installed():
+@click.option(
+    '-y', '--yes', is_flag=True, default=False,
+    help="Update all components without asking"
+)
+def update_installed(yes=None):
     """ Update all installed flowtool components. """
 
-    to_upgrade = []
-    for dist in get_extensions():
-        if click.confirm(' '.join([
-            colors.white('== Update'),
-            colors.cyan(dist.project_name),
-            colors.white('?')
-        ]), default=True):
-            to_upgrade.append(dist.project_name)
+    if yes:
+        to_upgrade = [d.project_name for d in get_extensions()]
+    else:
+        to_upgrade = []
+        for dist in get_extensions():
+            if click.confirm(' '.join([
+                colors.white('== Update'),
+                colors.cyan(dist.project_name),
+                colors.white('?')
+            ]), default=True):
+                to_upgrade.append(dist.project_name)
 
+    pip_args = ['--no-cache-dir', 'install', '--upgrade'] + to_upgrade
 
     echo.bold(colors.green(
-        ' '.join(['pip', 'install', '--upgrade'] + to_upgrade)
+        ' '.join(['pip'] + pip_args)
     ))
-    pip.main(['--no-cache-dir', 'install', '--upgrade'] + to_upgrade)
+    sys.exit(
+        pip.main(pip_args)
+    )
