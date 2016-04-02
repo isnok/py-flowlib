e08689cc3f01fa58211e25f6c35c07a4b805992d
Konstantin Martini <k@martini.pm>
2016-04-02 20:36:06 +0200

    closer to a aut release cycle

---
+++ base/flowtool/python.py
@@ -0,0 +1,24 @@
+
+def import_file(name, path):
+    """ A compatible way to import a module from its file name. """
+    try: # py3.5
+        import importlib.util
+        spec = importlib.util.spec_from_file_location(name, path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+    except:
+        pass
+
+    try: # py3.3, py3.4
+        from importlib.machinery import SourceFileLoader
+        module = SourceFileLoader(name, path).load_module()
+        return module
+    except:
+        pass
+
+    try: # py2
+        import imp
+        return imp.load_source(name, path)
+    except:
+        pass
--- versioning/flowtool_versioning/release.py
+++ versioning/flowtool_versioning/release.py
@@ -1,15 +1,34 @@
 import sys
 import click
 from flowtool.execute import run_command
-from flowtool.files import import_file
 from flowtool.style import colors, echo
+
+def version_or_exit():
+    get_version = run_command('./versioning.py')
+    if get_version.returncode:
+        echo.bold(colors.red('versioning.py'), 'was not found in current directory.')
+        sys.exit(1)
+    else:
+        return get_version.stdout.strip()
+
+def current_tag():
+    return run_command('git describe --tags').stdout.strip()
+
+def do_publish():
+    published = run_command('./setup.py sdist release')
+    if published.returncode:
+        echo.bold(colors.red('Failed:'))
+        echo.white(published.stdout)
+        echo.yellow(published.stderr)
+        sys.exit(published.returncode)
+    else:
+        echo.bold(colors.green('New release published.'))
 
 @click.command()
 def do_release():
     """ A proposed way to release with versioning. """
 
-    versioning = import_file('versioning', 'versioning.py')
-    auto_version = versioning.get_version()
+    auto_version = version_or_exit()
     dirty = 'dirty' in auto_version
 
     if dirty:
@@ -18,7 +37,7 @@ def do_release():
         sys.exit(1)
 
     echo.bold('Tag-Version check passed:', colors.green(auto_version))
-    echo.bold('Bumping version.')
+    echo.bold('Bumping version... ', nl=False)
 
     bump_result = run_command('./setup.py bump')
     if bump_result.returncode:
@@ -26,7 +45,16 @@ def do_release():
         echo.bold(colors.red(bump_result.stderr))
         sys.exit(bump_result.returncode)
 
-    auto_version = versioning.get_version()
-    echo.bold('Tag-Version now:', colors.green(auto_version))
+    auto_version = version_or_exit()
+    echo.bold('version is now:', colors.green(auto_version))
 
-
+    tag = current_tag()
+    message = colors.bold('Do the release? (tag: %s)' % tag)
+    if click.confirm(message):
+        do_publish()
+    else:
+        rollback = run_command(['git', 'tag', '-d', current_tag()])
+        if rollback.returncode:
+            echo.bold(colors.red(rollback))
+            sys.exit(rollback.returncode)
+        echo.cyan('Done:', rollback.stdout.strip())
