997aec3d1764734c4da568253594403bbf1136fa
Konstantin Martini <k@martini.pm>
2016-04-04 01:15:38 +0200

    inserting via symlink is now possible

--- githooks/flowtool_githooks/insert.py
+++ githooks/flowtool_githooks/insert.py
@@ -1,9 +1,12 @@
 import os
+import sys
 import click
 
-from flowtool.files import is_executable, toggle_executable
+from flowtool.files import is_executable, toggle_executable, find_parent_containing
 from flowtool.style import echo, colors
 from flowtool.ui import ask_choice
+
+from flowtool_git.common import local_repo
 
 from .manager import hook_specs
 
@@ -14,8 +17,9 @@ from .manager import hook_specs
 def introduce_githook(script, hook=None, copy=None):
     """ Install a file (script) as git hook. """
 
+    script_name = os.path.basename(script)
     if not hook:
-        heading = ['Install', colors.cyan(os.path.basename(script)), 'into which git hook?']
+        heading = ['Install', colors.cyan(script_name), 'into which git hook?']
         hook = ask_choice(
             heading=' '.join(heading),
             choices=hook_specs,
@@ -24,11 +28,27 @@ def introduce_githook(script, hook=None, copy=None):
 
     echo.white(
         'Will install',
-        colors.green(script),
+        colors.green(script_name),
         'as',
-        colors.cyan(hook),
+        colors.blue(hook),
         'using',
         colors.yellow('copy' if copy else 'symlink'),
         'installation method.',
     )
 
+    hook_dest = os.path.join(
+        local_repo().git_dir,
+        'hooks',
+        hook + '.d',
+        script_name,
+    )
+
+    echo.white('Hook source:', colors.cyan(script))
+    echo.white('Destination:', colors.cyan(hook_dest))
+
+    if copy:
+        os.link(script, hook_dest)
+    else:
+        os.symlink(script, hook_dest)
+
+    echo.green('Done.')
