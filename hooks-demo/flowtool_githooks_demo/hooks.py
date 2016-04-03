6e9db6bd93adf585bc2d6f7f48d4208e2d4da621
Konstantin Martini <k@martini.pm>
2016-04-03 02:33:20 +0200

    now the hook can show something

--- hooks-demo/flowtool_githooks_demo/hooks.py
+++ hooks-demo/flowtool_githooks_demo/hooks.py
@@ -2,9 +2,17 @@ import sys
 import click
 from flowtool.style import echo, colors
 from flowtool_git.common import local_repo
+from flowtool_git.common import short_status
 
 def demo(*args, **kwd):
-    echo.bold('demo-hook: Hey, this works!', local_repo().git_dir)
+    echo.bold('demo-hook:', local_repo().git_dir)
+    #echo.white('git status:')
+    for line in short_status():
+        if line.on_index != ' ':
+            echo.cyan(line.on_index, '', line.filename)
+        if line.untracked != ' ':
+            echo.yellow('', line.untracked, line.filename)
+
 
 def demo_setup(cmd=None):
     """ Micro setup.py imitation to set up configs or so. """
