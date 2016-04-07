5981d28670527d26233190c5bc0f5fcb6d4e6abb
Konstantin Martini <k@martini.pm>
2016-04-07 04:20:19 +0200

    click.commands for git hooks

--- hooks-demo/flowtool_githooks_demo/hooks.py
+++ hooks-demo/flowtool_githooks_demo/hooks.py
@@ -19,11 +19,13 @@ def read_stdin_nonblocking():
         return
     #stdin_was_closed()
 
-def demo(*args, **kwd):
+@click.command()
+@click.argument('args', nargs=-1)
+def demo(args):
     """ A demo git hook function.
         It tries to take all input a git hook can get and display it.
     """
-    echo.bold('demo-hook:', sys.argv[0], args, kwd)
+    echo.bold('demo-hook:', sys.argv[0], args)
     echo.white('git-dir:', local_repo().git_dir)
     #echo.white('git status:')
     for line in short_status():
@@ -44,9 +46,11 @@ def demo_setup(cmd=None):
 
 
 
-def demo_fail(*args, **kwd):
+@click.command()
+@click.argument('args', nargs=-1)
+def demo_fail(args=()):
     """ A hook that will always exit with a nonzero status. """
-    echo.red('demo-fail: OaaH, this will crash!', args, kwd)
+    echo.red('demo-fail: OaaH, this will crash!', args)
     stdin = ''.join(read_stdin_nonblocking())
     if stdin:
         echo.cyan('>>>stdin>>>')
