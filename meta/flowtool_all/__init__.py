f8d962e2a779f3acc7b4accdfc55aef71f219f93
Konstantin Martini <k@martini.pm>
2016-04-11 04:56:55 +0200

    added a test in meta pkg

--- meta/flowtool_all/__init__.py
+++ meta/flowtool_all/__init__.py
@@ -1,3 +1,25 @@
+""" Metapackage flowtool-all.
+
+    Let's try some 'integration tests' :-)
+
+    >>> command_names = (
+    ...     'clean-pycs',
+    ...     'feature',
+    ...     'githooks',
+    ...     'release',
+    ...     'self',
+    ...     'stage',
+    ...     'versioning',
+    ... )
+    >>> from flowtool.execute import run_command
+    >>> result = run_command('flowtool')
+    >>> result.returncode
+    0
+    >>> all(pkg in result.stdout for pkg in command_names)
+    True
+    >>> bool(result.stderr)
+    False
+"""
 
 from ._version import get_version
 __version__ = get_version()
