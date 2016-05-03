e5cf54ff1d1fb701dd831aec19acdbd21da21a27
Konstantin Martini <k@martini.pm>
2016-05-03 15:14:00 +0200

    clean version of base

--- base/setup.py
+++ base/setup.py
@@ -89,8 +89,8 @@ setup_args.update(
 setup_args.update(
     entry_points={
         'console_scripts': [
-            'flowtool = flowtool.main:flowtool_main_group',
-            'ft = flowtool.main:flowtool_main_group',
+            'flowtool = flowtool.main:main',
+            'ft = flowtool.main:main',
         ],
         'flowtool_commands': [
             'self-info = flowtool.info:show_info',
--- base/tests/test_main.py
+++ base/tests/test_main.py
@@ -8,7 +8,8 @@ runner = CliRunner()
 def test_noargs():
     result = runner.invoke(flowtool_main_group, ())
     assert result.exit_code == 0
-    assert 'flowtool' in result.output
+    assert 'self-info' in result.output
+    assert 'self-update' in result.output
 
 def test_info():
     result = runner.invoke(flowtool_main_group, ('self-info'))
@@ -16,16 +17,17 @@ def test_info():
     assert 'flowtool' in result.output
 
 def test_debug():
-    result = runner.invoke(flowtool_main_group, ('self-info'))
+    result = runner.invoke(flowtool_main_group, ('--debug', 'self-info'))
     assert result.exit_code in (0, 2)
-    assert 'flowtool' in result.output
+    assert 'Options:' in result.output
 
     # After this, debug stay enabled during the test session,
     # in the doctests if we don't disable it again:
 
     result = runner.invoke(flowtool_main_group, ('--no-debug', 'self-info'))
     assert result.exit_code in (0, 2)
-    assert 'flowtool' in result.output
+    assert 'Options:' not in result.output
+
 
 from flowtool.main import add_main_group_options, add_commands
 
